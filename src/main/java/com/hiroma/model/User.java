eaebfba2ab011b432ca75d31487bad5e496c47f6
dibyanibasnet941 <dibyanibasnet941@gmail.com>
2026-05-20 11:26:57 +0545

    Add model class

--- src/main/java/com/hiroma/model/User.java
+++ src/main/java/com/hiroma/model/User.java
@@ -1,5 +1,24 @@
 package com.hiroma.model;
 
 public class User {
+    private int userId;
+    private String fullName;
+    private String email;
+    private String phone;
+    private String role;
 
+    public int getUserId() { return userId; }
+    public void setUserId(int userId) { this.userId = userId; }
+
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 }
